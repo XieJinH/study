a5f83ffc320830c500ab59dca80335dd59915c9b
York.Yanmeng@gmail.com <YorkYanmeng233>
2022-12-04 23:42:40 +0800

    2022/12/04

--- C++/TopologicalSort.cpp
+++ C++/TopologicalSort.cpp
@@ -1,21 +1,60 @@
-#include "iostream"
-#include "queue"
-#include "vector"
+#include <bits/stdc++.h>
 
 using namespace std;
 
-// 判断拓扑排序是否成立
-bool TopSort(vector<vector<int>> &G, int n, vector<int> &inDegree) {
-    
+#define MAX_LENGTH 100
+
+vector<int> G[MAX_LENGTH];
+int n, m, inDegree[MAX_LENGTH];
+stack<int> s;
+
+bool topSort() {
+    int num = 0;
+    for (int i = 1; i <= n; i++) {
+        if (inDegree[i] == 0) {
+            s.push(i);
+        }
+    }
+
+    while (!s.empty()) {
+        int u = s.top();
+        cout << u << " ";
+        s.pop();
+
+        for (int i = 0; i < G[u].size(); i++) {
+            int v = G[u][i];
+            inDegree[v]--;
+
+            if (inDegree[v] == 0) {
+                s.push(v);
+            }
+        }
+
+        num++;
+    }
+
+    // 判断是否有环
+    if (num == n) return true;
+    else return false;
 }
 
 int main() {
-    int n, m;
     cin >> n >> m;
 
-    for (int i = 0 ; i < m; i++) {
+    // 初始化
+    memset(inDegree, 0, sizeof(inDegree));
 
+    for (int i = 1; i <= m; i++) {
+        int x, y;
+        cin >> x >> y;
+        inDegree[y]++;
+
+        // 头插法
+        if (G[x].empty()) G[x].push_back(y);
+        else G[x].insert(G[x].begin(), y);
     }
+
+    if (!topSort()) cout << endl << 0;
 
     return 0;
 }
