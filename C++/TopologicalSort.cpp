81dc239ae4c6c1a118906d277b85080622bf27db
York.Yanmeng@gmail.com <YorkYanmeng233>
2022-12-04 21:53:58 +0800

    2022/12/04

---
+++ C++/TopologicalSort.cpp
@@ -0,0 +1,21 @@
+#include "iostream"
+#include "queue"
+#include "vector"
+
+using namespace std;
+
+// 判断拓扑排序是否成立
+bool TopSort(vector<vector<int>> &G, int n, vector<int> &inDegree) {
+    
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0 ; i < m; i++) {
+
+    }
+
+    return 0;
+}
